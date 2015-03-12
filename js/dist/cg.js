d7bbadc2d2f9f2b964782a53c410fcd2f14b083c
aureooms <aurelien.ooms@gmail.com>
2015-03-12 11:06:35 +0100

    better comments

--- js/dist/cg.js
+++ js/dist/cg.js
@@ -1718,61 +1718,73 @@ exports.__pit__ = __pit__;
 
 
 /**
- * Ray casting algorithm.
- * ==
- *
- * Counts the number of time a ray intersects a polygon.
- *
- * Hypothesis :
- *  - polygon is represented as an array of vertices
- *  - polygon has at least 3 vertices
- *
- * @param {array} polygon array of vertices
- * @param {integer} i index of the first vertex of the polygon
- * @param {integer} j index of the last vertex of the polygon
- * @param {point} p first point of the ray
- * @param {point} q second point of the ray
- *
- * see http://en.wikipedia.org/wiki/Point_in_polygon
+ * @param {function} ris ris( p , q , u , v ) computes if ray pq intersects
+ * segement uv, implementation can differ for the corner case where p is on uv.
+ * Hence, one can generate two different versions of the ray casting algorithm
+ * depending if one wants to compute strict or non-strict polygon point
+ * containment. For strict containment ris should return 1 for p on uv and 0
+ * otherwise. For non-strict containment ris should return 0 for p on uv and 1
+ * otherwise.
  */
 
 var __rc__ = function ( ris ) {
 
-	var rc = function ( polygon, i, j, p, q ) {
+	/**
+	 * Ray casting algorithm.
+	 * ==
+	 *
+	 * Counts the number of times a ray intersects a polygon. If the count is a
+	 * multiple of 2 then the point lies outside the polygon, inside otherwise.
+	 *
+	 * Hypothesis :
+	 *  - polygon is represented as an array of vertices
+	 *  - polygon has at least 2 vertices
+	 *
+	 * @param {array} polygon array of vertices
+	 * @param {integer} i index of the first vertex of the polygon
+	 * @param {integer} j index past the last vertex of the polygon
+	 * @param {point} p origin point of the ray
+	 * @param {point} q direction point of the ray
+	 *
+	 * see http://en.wikipedia.org/wiki/Point_in_polygon
+	 */
 
-		var u, v, n;
+	var rc = function ( polygon , i , j , p , q ) {
 
-		n = 0;
+		var u , v , n ;
 
-		u = polygon[j-1];
-		v = polygon[i];
-		++i;
+		n = 0 ;
+
+		u = polygon[j-1] ;
+		v = polygon[i] ;
+
+		++i ;
 
 		for ( ; ; ) {
-			n += ris( p, q, u, v );
-			++i;
+			n += ris( p , q , u , v ) ;
+			++i ;
 
 			if ( i === j ) {
-				return n;
+				return n ;
 			}
 
-			u = polygon[i];
+			u = polygon[i] ;
 
-			n += ris( p, q, u, v );
-			++i;
+			n += ris( p , q , u , v ) ;
+			++i ;
 
 			if ( i === j ) {
-				return n;
+				return n ;
 			}
 
-			v = polygon[i];
+			v = polygon[i] ;
 		}
-	};
+	} ;
 
-	return rc;
-};
+	return rc ;
+} ;
 
-exports.__rc__ = __rc__;
+exports.__rc__ = __rc__ ;
 
 /* js/src/d2/pred/sinsign.js */
 
