2d62380ef946c663ea4ee4431144d6c428cb663f
aureooms <aurelien.ooms@gmail.com>
2015-09-23 17:14:13 +0000

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -151,6 +151,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "Children",
+            "slug": "children"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "Reference",
             "slug": "reference"
           },
